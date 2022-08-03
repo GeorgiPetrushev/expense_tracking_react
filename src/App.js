4b3d9dcf9c7c23f964c5aeefa6e589a723adff6d
Georgi <figoto660@gmail.com>
2022-08-03 17:31:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItam from "./components/ExspenseItam";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItam />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExspenseItam.js
@@ -0,0 +1,15 @@
+import "./ExspenseItam.css";
+
+function ExpenseItam() {
+  return (
+    <div>
+      <div>Data</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItam;
